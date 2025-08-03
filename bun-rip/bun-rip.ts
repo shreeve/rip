117ce85eb5a4984f43f1da0b24241742074927e3
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-02 23:38:13 -0600

    Some new beautiful clean awesomeness!

--- bun-rip/bun-rip.ts
+++ bun-rip/bun-rip.ts
@@ -1,18 +1,18 @@
 /// <reference types="bun-types" />
 
-import rip from "../coffeescript/lib/coffeescript";
+import { compile } from "../coffeescript/lib/coffeescript"
 
 Bun.plugin({
   name: "bun-rip",
   setup({ onLoad }) {
     onLoad({ filter: /\.rip$/ }, async ({ path }) => ({
       loader: "js",
-      contents: rip.compile(await Bun.file(path).text(), {
-        filename: path,
+      contents: compile(await Bun.file(path).text(), {
         bare: true,
         header: true,
+        filename: path,
         inlineMap: true,
       }),
-    }));
+    }))
   },
-});
+})
