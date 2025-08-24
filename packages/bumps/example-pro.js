0b77be8deec74025755b4ea4b06bf9d38fef9b81
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-24 11:13:07 -0600

    Outdated bumps code

--- packages/bumps/example-pro.js
+++
@@ -1,7 +0,0 @@
-import { parseMumps } from "./mumps-parser-pro.js";
-
-const src = 'HELLO  W "Hi",!  S A=1,B=2  I A?1A1N W "OK",!  H';
-const ast = parseMumps(src);
-
-console.log(ast.format());          // pretty print
-console.log(ast.symbols.get(1));    // first interned symbol (example)
