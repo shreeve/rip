b7ca98a449bd5624a77bb2dcc205d194e509bcc9
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-26 13:30:05 -0600

    Manage this separately for now

--- packages/bumps/sample.m
+++
@@ -1,5 +0,0 @@
-; sample.m — quick test file
-HELLO  W "Hi",!  S A=1,B(2)="ok"  I A?1A1N W "OK",!  H  ; greet
-SETTEST  S A=1,B(10)=2,C=333  ; align equals
-READLINE R X S Y=$L(X) W "LEN=",Y,!
-NEWLAB   N I,J S I=1,J=2  W I+J,!  Q
