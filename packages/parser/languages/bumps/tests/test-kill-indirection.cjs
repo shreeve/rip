a6dc8b83a68c2fcb1e94397abf2e198b8039579d
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 07:38:41 -0600

    tests: add KILL indirection cases (@NAME, @(expr)); suite green

---
+++ packages/parser/languages/bumps/tests/test-kill-indirection.cjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'KILL @X,@(Y+1)\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks; p.yy = { node: (t,p)=>({type:t,...(p||{})}) };
+  p.lexer = { all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(){ this.i=0; return this; },
+    lex(){ if(this.i>=this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylineno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  const ast = parserMod.parse(src);
+  const cmd = ast.lines[0].cmds[0];
+  assert.equal(cmd.op, 'KILL');
+  const [a,b] = cmd.args.items;
+  assert.equal(a.type, 'Indirect');
+  assert.equal(a.kind, 'name');
+  assert.equal(b.type, 'Indirect');
+  assert.equal(b.kind, 'expr');
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
