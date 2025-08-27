52ad6d61d9a2f511067822c93fec66ca5ee67442
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 01:58:32 -0600

    bumps: add more tests (negated relations, DO label+offset args, more generic commands, RHS indirect+string) (PASS)

---
+++ packages/parser/languages/bumps/tests/test-negated-relations.cjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'IF X' + "'[" + '"A"' + '\n' + 'IF X' + "']" + '1' + '\n' + 'IF X' + "']]" + 'Y' + '\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks; p.yy = { node: (t,p)=>({type:t,...(p||{})}) };
+  p.lexer = { all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(){ this.i=0; return this; },
+    lex(){ if(this.i>=this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylineno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  const ast = parserMod.parse(src);
+  assert.equal(ast.type, 'Program');
+  assert.equal(ast.lines.length, 3);
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
