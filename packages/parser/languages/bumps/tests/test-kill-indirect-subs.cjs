d647979abeb151a0864f9f9b48dd379db77d1666
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 08:11:24 -0600

    tests: add KILL subtree with indirect subscripts (@X, @(...), empty string) ; green

---
+++ packages/parser/languages/bumps/tests/test-kill-indirect-subs.cjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'KILL ^G(@X, "", @(Y+1))\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks; p.yy = { node: (t,p)=>({type:t,...(p||{})}) };
+  p.lexer = { all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(){ this.i=0; return this; },
+    lex(){ if(this.i>=this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylleno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  const ast = parserMod.parse(src);
+  const v = ast.lines[0].cmds[0].args.items[0];
+  assert.equal(v.type, 'Var');
+  assert.equal(v.global, true);
+  assert.equal(v.subs.length, 3);
+  assert.equal(v.subs[0].type, 'Indirect');
+  assert.equal(v.subs[1].type, 'String');
+  assert.equal(v.subs[2].type, 'Indirect');
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
