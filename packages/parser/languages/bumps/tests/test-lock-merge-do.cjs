061f6418fbf13d267f556bb5a7877da53bdd1f99
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 01:17:53 -0600

    bumps: add tests for SET/WRITE/READ and LOCK/MERGE/DO (PASS)

---
+++ packages/parser/languages/bumps/tests/test-lock-merge-do.cjs
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+const { attachBlocks } = require('../blocks.coffee');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'LOCK X:1,Y\nMERGE A=B\nDO ^ROUT(1,2)\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks;
+  p.yy = p.yy || {};
+  p.yy.node = p.yy.node || ((type, props) => ({ type, ...(props || {}) }));
+  p.lexer = {
+    all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(input, yy){ this.i=0; return this; },
+    lex(){ if (this.i >= this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylineno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  let ast = parserMod.parse(src);
+  if (ast && ast.type === 'Program') ast = attachBlocks(ast);
+  assert.equal(ast.type, 'Program');
+  // LOCK
+  const l1 = ast.lines[0];
+  assert.equal(l1.cmds[0].op, 'LOCK');
+  assert.equal(l1.cmds[0].args.length, 2);
+  assert.equal(l1.cmds[0].args[0].timeout.value, 1);
+  // MERGE
+  const l2 = ast.lines[1];
+  assert.equal(l2.cmds[0].op, 'MERGE');
+  // DO
+  const l3 = ast.lines[2];
+  assert.equal(l3.cmds[0].op, 'DO');
+  assert.equal(l3.cmds[0].args.targets[0].routine, 'ROUT');
+  assert.equal(l3.cmds[0].args.targets[0].label, null);
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
---
+++ packages/parser/languages/bumps/tests/test-set-write-read.cjs
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+const { attachBlocks } = require('../blocks.coffee');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'SET X=1,Y=2\nWRITE "A", 1, X\nREAD X:5\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks;
+  p.yy = p.yy || {};
+  p.yy.node = p.yy.node || ((type, props) => ({ type, ...(props || {}) }));
+  p.lexer = {
+    all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(input, yy){ this.i=0; return this; },
+    lex(){ if (this.i >= this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylineno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  let ast = parserMod.parse(src);
+  if (ast && ast.type === 'Program') ast = attachBlocks(ast);
+  assert.equal(ast.type, 'Program');
+  assert.equal(ast.lines.length, 3);
+  // SET
+  const setLine = ast.lines[0];
+  assert.equal(setLine.cmds[0].op, 'SET');
+  assert.equal(setLine.cmds[0].args.items.length, 2);
+  // WRITE
+  const wLine = ast.lines[1];
+  assert.equal(wLine.cmds[0].op, 'WRITE');
+  assert.equal(wLine.cmds[0].args.items.length, 3);
+  // READ
+  const rLine = ast.lines[2];
+  assert.equal(rLine.cmds[0].op, 'READ');
+  assert.equal(rLine.cmds[0].args.items[0].timeout.value, 5);
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
