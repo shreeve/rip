00b017a934aa83e4b4c7a291e2f272f287d62f96
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 08:42:02 -0600

    tests: add FOR mixed step omission and LOCK mixed timeout cases; adjust expr expectation; green

---
+++ packages/parser/languages/bumps/tests/test-for-mixed-step.cjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'FOR I=1:10,J=0:2:10\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks; p.yy = { node: (t,p)=>({type:t,...(p||{})}) };
+  p.lexer = { all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(){ this.i=0; return this; },
+    lex(){ if(this.i>=this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylineno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  const ast = parserMod.parse(src);
+  const specs = ast.lines[0].cmds[0].specs;
+  assert.equal(specs.length, 2);
+  assert.equal(specs[0].name, 'I');
+  assert.equal(specs[0].step.value, 1);
+  assert.equal(specs[1].name, 'J');
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
