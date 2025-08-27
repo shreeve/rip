00b08b7532b80d9bf20ea6993e7d33dd2ded65b5
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 07:16:40 -0600

    tests: add unclosed pattern error; regenerate parser after restoring primary: NAME; green

---
+++ packages/parser/languages/bumps/tests/test-error-pattern-unclosed.cjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const src = 'IF X?2(1A,1N\n';
+  let threw = false;
+  try {
+    parserMod.parse(src);
+  } catch (e) {
+    threw = true;
+  }
+  assert.equal(threw, true);
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
