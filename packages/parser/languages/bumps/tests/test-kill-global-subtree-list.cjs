26e5a75d746afb9cb644d63213ecc107f888969d
Steve Shreeve <steve.shreeve@gmail.com>
2025-08-27 08:13:11 -0600

    tests: add deeper NEW nested mixed and KILL global subtree list cases; green

---
+++ packages/parser/languages/bumps/tests/test-kill-global-subtree-list.cjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const parserMod = require('../parser.cjs');
+require('../../../../../coffeescript/register.js');
+
+(async () => {
+  const { BumpsLexer } = await import('../lexer.js');
+  const src = 'KILL ^G(1,2),^H(3),^I\n';
+  const lex = new BumpsLexer();
+  const toks = lex.tokenize(src);
+  const p = parserMod.parser;
+  p.tokens = toks; p.yy = { node: (t,p)=>({type:t,...(p||{})}) };
+  p.lexer = { all: toks, i: 0, yytext: '', yyleng: 0, yylloc: {}, yylineno: 0,
+    setInput(){ this.i=0; return this; },
+    lex(){ if(this.i>=this.all.length) return 1; const [t,v,l]=this.all[this.i++]; this.yytext=String(v??t); this.yyleng=this.yytext.length; this.yylloc=l||{}; this.yylleno=this.yylloc.first_line||0; return t; },
+    showPosition(){ return ''; }
+  };
+  const ast = parserMod.parse(src);
+  const it = ast.lines[0].cmds[0].args.items;
+  assert.equal(it.length, 3);
+  assert.equal(it[0].type, 'Var');
+  assert.equal(it[0].global, true);
+  assert.equal(it[1].type, 'Var');
+  assert.equal(it[1].global, true);
+  assert.equal(it[2].type, 'Var');
+  assert.equal(it[2].global, true);
+  console.log('PASS');
+})().catch(e => { console.error('FAIL', e); process.exit(1); });
+
+
