86968be2ac2eedbd06cd01f90ebbaddf56f88ddd
Steve Shreeve <steve.shreeve@gmail.com>
2025-10-23 19:19:26 -0600

    Remove the vanilla coffeescript code

--- coffeescript/lib/coffeescript/parser-wrapper.js
+++
@@ -1,6 +0,0 @@
-// ESM wrapper for the generated parser
-var parserModule, ref;
-
-parserModule = require('./parser');
-
-export var parser = (ref = parserModule.parser) != null ? ref : parserModule;
